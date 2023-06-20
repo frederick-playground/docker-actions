546401aa534be0eda18c4d1966dce23db265ffad
Frederick Wong <77422662+frederickcwong@users.noreply.github.com>
2023-06-19 18:14:41 -0700

    test get latest

--- test.sh
+++ test.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "anyone home?"
+trigger=$1
+updated=$2
+
+echo $trigger $updated
+
+if [[ ((($trigger = 'workflow_dispatch') || \
+        ($trigger = 'schedule')) && \
+       ($updated = 'true')) || \
+      ($trigger == "push") ]]
+then
+    echo "run"
+else
+    echo "dont run"
+fi
